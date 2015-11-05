ef798f9dbb98ec6eed2fb3f0cfff063f3f8d3aa2
bmomen <babakmomen@mail.com>
2015-11-05 11:48:58 +0000

    Recommit old project.

---
+++ BabakMVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BabakMVC5.Startup))]
+namespace BabakMVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
